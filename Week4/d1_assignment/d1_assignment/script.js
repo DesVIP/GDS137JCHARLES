3011fd2a0c99fbb36b17ae534dc3f4b77effc434
Jordan Charles <jccharles@email.neit.edu>
2022-11-09 14:30:37 -0500

    Week 4 Hw

---
+++ Week4/d1_assignment/d1_assignment/script.js
@@ -0,0 +1,31 @@
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext('2d')
+//Line
+ctx.lineWidth ="5px"
+ctx.beginPath();
+ctx.moveTo(278,549);
+ctx.lineTo(87,682);
+ctx.stroke() 
+
+
+//Square
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
