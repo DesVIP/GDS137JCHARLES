6df151ad0c803360faaf36f0d8e52923f11b9fde
Jordan Charles <jccharles@email.neit.edu>
2023-04-07 11:26:40 -0400

    Wowow

---
+++ Pweek 1/js/week1_basic_movement.js
@@ -0,0 +1,48 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new Player();
+	player.vx = -70;
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	//Move the Player
+	player.move();
+
+	//Collision
+	if(player.x < player.width/2)
+	{
+		player.x = player.width/2
+		player.vx = 0//-player.vx;
+	}
+
+	if(player.x > canvas.width - player.width/2)
+	{
+		player.x = canvas.width - player.width/2
+		player.vx = 0//-player.vx;
+	}
+	
+	//Update the Screen
+	player.draw();
+}
+
+functionmove(ob)
+{
+object.x += object.vx
+}
