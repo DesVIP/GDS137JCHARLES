ff050179fa4386bc05ada719ab9a2527c01e6e33
Jordan Charles <jccharles@email.neit.edu>
2023-11-13 14:13:06 -0500

    .

--- GDS137/level0/level0mb/Week2/js/week2_keyboard_movement.js
+++ GDS137/level0/level0mb/Week2/js/week2_keyboard_movement.js
@@ -37,24 +37,30 @@ function animate()
 	if(w)
 	{
 		console.log("Moving Up");
-		player.y += -2;
+		player.y += 2;
 	}
 	if(s)
 	{
-		console.log("Moving Down");
-		player.y += 2;
+		console.log("Moving Down")
+		player.y += -2;
 	}
 
 	//Boundaries
 
-	//The Up Boundary
-	if(player.y < player.height - canvas.height)
+	//---------------Bounce of Up------------------------
+	if(player.y < player.height+750 - canvas.height)
 	{
 		player.vy = -player.vy;
 		player.y < player.height - canvas.height;
-		
 	}
 
+	//---------------Bounce of Down----------------------
+    if(player.y > canvas.height+50 - player.height)
+	{
+		player.vy = -player.vy
+		player.y < player.height - canvas.height;
+	}
+	
 	//Update the Screen
 	player.drawRect();
 }
